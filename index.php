05ea21004e65f868524756f416da5fdaf91c7351
James William M. Tarrobal <james.tarrobal.21@gmail.com>
2018-05-02 14:49:14 +0800

    Index page update
    Slight UI improvements

--- index.php
+++ index.php
@@ -1,174 +1,70 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
 <!DOCTYPE html>
 <html lang="en">
-
-<?php
-$connection = new MongoClient( "mongodb://localhost" );
-$db = $connection->project_sql;
-?>
-
 <head>
-<title>main</title>
- <!-- Meta-Tags -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta charset="utf-8">
-    <meta name="keywords" content="Elegant Subscribe Form a Responsive Web Template, Bootstrap Web Templates, Flat Web Templates, Android Compatible Web Template, Smartphone Compatible Web Template, Free Webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+  <title>Tumblr</title>
+  <!-- Meta-Tags -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="keywords" content="Elegant Subscribe Form a Responsive Web Template, Bootstrap Web Templates, Flat Web Templates, Android Compatible Web Template, Smartphone Compatible Web Template, Free Webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design">
+  <script>
+    addEventListener("load", function () {
+      setTimeout(hideURLbar, 0);
+    }, false);
 
-    <script>
-        addEventListener("load", function () {
-            setTimeout(hideURLbar, 0);
-        }, false);
+    function hideURLbar() {
+      window.scrollTo(0, 1);
+    }
+  </script>
+  <!-- //Meta-Tags -->
 
-        function hideURLbar() {
-            window.scrollTo(0, 1);
-        }
-    </script>
-    <!-- //Meta-Tags -->
-
-	<!-- css files -->
-	<link rel="stylesheet" href="web/css/login.css" type="text/css" media="all" /> <!-- Style-CSS -->
-	<!-- //css files -->
-	<!-- web-fonts -->
-	<link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
-	<!-- //web-fonts -->
-	<style>
-		body {
-  background-color: #012839;
-  text-align: center;
-  padding-top: 50px;
-}
-
-.button {
-  text-decoration: none;
-  text-transform: uppercase;
-  font-family: 'Exo 2', sans-serif;
-  font-weight: 300;
-  font-size: 30px;
-  display: inline-block;
-  position: relative;
-  text-align: center;
-  color: #00c7ec;
-  border: 1px solid #00c7ec;
-  border-radius: 5px;
-  line-height: 3em;
-  padding-left: 5em;
-  padding-right: 5em;
-  box-shadow: 0 0 0 0 transparent;
-  -webkit-transition: all 0.2s ease-in;
-  -moz-transition: all 0.2s ease-in;
-  transition: all 0.2s ease-in;
-}
-.button:hover {
-  color: white;
-  box-shadow: 0 0 30px 0 rgba(0, 199, 236, 0.5);
-  background-color: #00c7ec;
-  -webkit-transition: all 0.2s ease-out;
-  -moz-transition: all 0.2s ease-out;
-  transition: all 0.2s ease-out;
-}
-.button:hover:before {
-  -webkit-animation: shine 0.5s 0s linear;
-  -moz-animation: shine 0.5s 0s linear;
-  animation: shine 0.5s 0s linear;
-}
-.button:active {
-  box-shadow: 0 0 0 0 transparent;
-  -webkit-transition: box-shadow 0.2s ease-in;
-  -moz-transition: box-shadow 0.2s ease-in;
-  transition: box-shadow 0.2s ease-in;
-}
-.button:before {
-  content: '';
-  display: block;
-  width: 0px;
-  height: 86%;
-  position: absolute;
-  top: 7%;
-  left: 0%;
-  opacity: 0;
-  background: white;
-  box-shadow: 0 0 15px 3px white;
-  -webkit-transform: skewX(-20deg);
-  -moz-transform: skewX(-20deg);
-  -ms-transform: skewX(-20deg);
-  -o-transform: skewX(-20deg);
-  transform: skewX(-20deg);
-}
-
-@-webkit-keyframes shine {
-  from {
-    opacity: 0;
-    left: 0%;
-  }
-  50% {
-    opacity: 1;
-  }
-  to {
-    opacity: 0;
-    left: 100%;
-  }
-}
-@-moz-keyframes shine {
-  from {
-    opacity: 0;
-    left: 0%;
-  }
-  50% {
-    opacity: 1;
-  }
-  to {
-    opacity: 0;
-    left: 100%;
-  }
-}
-@keyframes shine {
-  from {
-    opacity: 0;
-    left: 0%;
-  }
-  50% {
-    opacity: 1;
-  }
-  to {
-    opacity: 0;
-    left: 100%;
-  }
-}
-
-	</style>
-
+  <!-- css files -->
+  <link rel="stylesheet" href="web/css/login.css" type="text/css" media="all" /> <!-- Style-CSS -->
+  <!-- //css files -->
+  <!-- web-fonts -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+  <!-- //web-fonts -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 </head>
 <body>
-		<!--header-->
-		<div class="header-w3l">
-			<h1></h1>
-		</div>
-		<!--//header-->
+  <!--header-->
+  <div class="header-w3l">
+    <h1>Tumblr</h1>
+  </div>
+  <!--//header-->
 
-		<!--main-->
-		<div class="content">
+  <!--main-->
+  <div class="content">
+    <div class="main-content-agile">
+      <div class="wthree-pro">
+        <h2>WELCOME</h2>
+        <h3>Enroll for our <span>free</span> updates</h3>
+        <li>We will send you weekly updates</li>
+        <li>Get daily articles and latest news</li>
+      </div>
+      <div class="sub-main-w3">
+        <form action="web/userLog/login.php">
+          <input type="submit" value="Sign in">
+          <p><a href="web/userLog/register.php" class="btn btn-link btn-block"><u>Sign up</u></a></p>
+        </form>
+      </div>
+    </div>
+  </div>
+  <!--//main-->
 
-			<div class="wthree-pro">
-				<h2></h2>
-				<h3>Welcome to <span>Tumblr</span> updates</h3>
-				<li>We will send you weekly updates</li>
-				<li>Get daily articles and latest news</li>
-			</div>
+  <!--footer-->
+  <div class="footer">
 
-			<p><a href="web/userLog/login.php" class="button">  Login!</a> </p>
-			<p><a href="web/userLog/register.php" class="button">Register!</a></p>
+  </div>
+  <!--//footer-->
 
-		</div>
-		<!--//main-->
-
-		<!--footer-->
-		<div class="footer">
-
-		</div>
-		<!--//footer-->
-
-	</body>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+</body>
 </html>
